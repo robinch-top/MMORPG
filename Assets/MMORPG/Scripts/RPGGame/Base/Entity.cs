f1610c1ee1bc589eea4b9bd5f722082b91b31276
ChenBin <chenbin@dongmengwl.com>
2021-02-25 15:07:17 +0800

    初步资源

---
+++ Assets/MMORPG/Scripts/RPGGame/Base/Entity.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Assets.MMORPG.Scripts.RPGGame.Base
+{
+    public class Entity : MonoBehaviour
+    {
+        void Awake()
+        {
+
+        }
+        // Use this for initialization
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
